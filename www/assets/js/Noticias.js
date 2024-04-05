0fdd71d52af9e7c31da933502cb2fa3ff0ad85d3
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-04-05 17:50:59 -0300

    feito o padrao de cada pagina

--- www/assets/js/Noticias.js
+++ www/assets/js/Noticias.js
@@ -1,57 +1,55 @@
 document.addEventListener('DOMContentLoaded', function () {
-    if (window.innerWidth < 1023) {
-        const sliders = document.querySelectorAll('.slider');
-        const setasAnteriores = document.querySelectorAll('.SetaAnterior');
-        const setasProximas = document.querySelectorAll('.SetaProxima');
+    const sliders = document.querySelectorAll('.slider');
+    const setasAnteriores = document.querySelectorAll('.SetaAnterior');
+    const setasProximas = document.querySelectorAll('.SetaProxima');
 
-        sliders.forEach((slider, index) => {
-            const slides = slider.querySelectorAll('span');
-            let numSlides = slides.length;
-            if (window.innerWidth > 767) {
-                numSlides = slides.length - 1;
-            }
-            let currentIndex = 0;
-            let interval;
-
+    sliders.forEach((slider, index) => {
+        const slides = slider.querySelectorAll('span');
+        let numSlides = slides.length;
+        if (window.innerWidth > 767) {
+            numSlides = slides.length - 1;
+        }
+        let currentIndex = 0;
+        let interval;
 
 
-            function atualizarSlider() {
-                const slideWidth = slides[currentIndex].offsetWidth;
-                const translateValue = -currentIndex * (slideWidth + 20);
-                slider.style.transform = `translateX(${translateValue}px)`;
-            }
 
-            function nextSlide() {
-                currentIndex = (currentIndex + 1) % numSlides;
-                atualizarSlider();
-            }
+        function atualizarSlider() {
+            const slideWidth = slides[currentIndex].offsetWidth;
+            const translateValue = -currentIndex * (slideWidth + 20);
+            slider.style.transform = `translateX(${translateValue}px)`;
+        }
 
-            function prevSlide() {
-                currentIndex = (currentIndex - 1 + numSlides) % numSlides;
-                atualizarSlider();
-            }
+        function nextSlide() {
+            currentIndex = (currentIndex + 1) % numSlides;
+            atualizarSlider();
+        }
 
-            setasAnteriores[index].addEventListener('click', () => {
-                prevSlide();
-                clearInterval(interval);
-                interval = setInterval(nextSlide, 5000);
-            });
+        function prevSlide() {
+            currentIndex = (currentIndex - 1 + numSlides) % numSlides;
+            atualizarSlider();
+        }
 
-            setasProximas[index].addEventListener('click', () => {
-                nextSlide();
-                clearInterval(interval);
-                interval = setInterval(nextSlide, 5000);
-            });
-
-            interval = setInterval(() => {
-                nextSlide();
-                if (currentIndex === slides.length - 1) {
-                    setTimeout(() => {
-                        currentIndex = 0;
-                        atualizarSlider();
-                    }, 1000);
-                }
-            }, 5000);
+        setasAnteriores[index].addEventListener('click', () => {
+            prevSlide();
+            clearInterval(interval);
+            interval = setInterval(nextSlide, 5000);
         });
-    }
+
+        setasProximas[index].addEventListener('click', () => {
+            nextSlide();
+            clearInterval(interval);
+            interval = setInterval(nextSlide, 5000);
+        });
+
+        interval = setInterval(() => {
+            nextSlide();
+            if (currentIndex === slides.length - 1) {
+                setTimeout(() => {
+                    currentIndex = 0;
+                    atualizarSlider();
+                }, 1000);
+            }
+        }, 5000);
+    });
 });
