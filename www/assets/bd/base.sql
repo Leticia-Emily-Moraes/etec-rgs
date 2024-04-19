93515670387095c1542a0454f194ba704120ff7f
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2024-04-19 20:44:13 -0300

    fix: alterações no banco de dados

--- www/assets/bd/base.sql
+++ www/assets/bd/base.sql
@@ -8,21 +8,41 @@ CREATE TABLE usuarios (
     IdUsuario INT AUTO_INCREMENT PRIMARY KEY,
     NomeCompleto VARCHAR(100) NOT NULL,
     NomeUser VARCHAR(50) NOT NULL,
-    SenhaUser VARCHAR(255) NOT NULL,
-    Cargo VARCHAR(50) NOT NULL
+    SenhaUser VARCHAR(100) NOT NULL, 
+    Cargo VARCHAR(50) NOT NULL,
+    UNIQUE (NomeUser), 
+    INDEX (NomeUser)
 );
 
 CREATE TABLE noticias (
 	IdNoticia INT AUTO_INCREMENT NOT NULL,
     Titulo VARCHAR(50) NOT NULL,
-    Imagem BLOB NOT NULL,
-    Descricao VARCHAR(255) NOT NULL,
+    Resumo VARCHAR(100) NOT NULL,
+    Categoria VARCHAR(50) NOT NULL,
+    ImagemCapa VARCHAR(255) NOT NULL,
     Autor INT NOT NULL,
     HoraPublicado DATETIME NOT NULL DEFAULT NOW(),
     FOREIGN KEY (Autor) REFERENCES usuarios(IdUsuario),
-PRIMARY KEY(IdNoticia)
+    PRIMARY KEY(IdNoticia)
 );
 
-INSERT INTO usuarios (NomeCompleto, NomeUser, SenhaUser, Cargo) VALUES ('Rodrigo Vicente','Root','Admin2024', 'Administrador');
+CREATE TABLE noticiasConteudo (
+	IdConteudo INT AUTO_INCREMENT NOT NULL,
+    FkNoticia INT NOT NULL,
+    Text1 VARCHAR(100) NOT NULL,
+    Imagem1 VARCHAR(255) NOT NULL,
+	Text2 VARCHAR(100),
+    Imagem2 VARCHAR(255),
+    Text3 VARCHAR(100),
+    Imagem3 VARCHAR(255),
+    Text4 VARCHAR(100),
+    Imagem4 VARCHAR(255),
+    FOREIGN KEY (FkNoticia) REFERENCES noticias(IdNoticia),
+    PRIMARY KEY(IdConteudo)
+);
+
+INSERT INTO usuarios (NomeCompleto, NomeUser, SenhaUser, Cargo) VALUES ('Rodrigo Vicente','Root', 'Admin2024', 'Administrador');
 
 SELECT * FROM usuarios;
+
+SELECT * FROM noticias;
